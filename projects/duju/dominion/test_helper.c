1e729d6a65b5a08930bb0c85c9e6ccded67df9b2
Juan Du <duju@oregonstate.edu>
2018-01-27 09:07:19 -0800

    Reorganize code

--- projects/duju/dominion/test_helper.c
+++ projects/duju/dominion/test_helper.c
@@ -1,12 +1,80 @@
 #include "test_helper.h"
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+
+int cmp(const void* a, const void* b) {
+  if (*(int*)a > *(int*)b)
+    return 1;
+  if (*(int*)a < *(int*)b)
+    return -1;
+  return 0;
+}
+
+int* removeElementFromArray(int* originalArray,int size, int handPos){
+  int* newArray = malloc((size-1)*sizeof(int)); 
+  int i = 0;
+  int j = 0;
+  if (handPos >= size) return NULL;
+  for (; i<size; ++i){
+    if (i != handPos){
+      newArray[j] = originalArray[i];
+      ++j;
+    }
+  }
+  return newArray;
+
+}
+
+void printGameState(struct gameState* G, int player){
+
+   int i;
+   printf("The player is %d\n", player); 
+   printf("The turn is %d\n", G->whoseTurn); 
+   printf("Handcounts: %d\n", G->handCount[player]); 
+   printf("current hand is: "); 
+   for (i=0; i<G->handCount[player]; ++i){
+     printf(" %d\t", G->hand[player][i]); 
+   }
+   printf("\n");
+
+   printf("Deckcounts: %d\n", G->deckCount[player]); 
+   printf("current deck is: "); 
+   for (i=0; i<G->deckCount[player]; ++i){
+     printf("%d\t", G->deck[player][i]); 
+   }
+   printf("\n");
+
+   printf("discardCounts: %d\n", G->discardCount[player]); 
+   printf("current discard is: "); 
+   for (i=0; i<G->discardCount[player]; ++i){
+     printf("%d\t", G->discard[player][i]); 
+   }
+   printf("\n");
+
+   printf("playedcard count is: %d\n", G->playedCardCount); 
+   printf("played card is: "); 
+   for (i=0; i<G->playedCardCount; ++i){
+     printf("%d\t", G->playedCards[i]); 
+   }
+   printf("\n");
+}
 void asserttrue(bool statement){
 
   if (statement)
     printf("TEST SUCCESSFULLY COMPLETED!\n");
   else 
     printf("TEST FAILED!!!!\n");
+}
 
+bool isSameArray(int* array1, int* array2, int size){
+  qsort ((void*)(array1), size, sizeof(int), cmp); 
+  qsort ((void*)(array2), size, sizeof(int), cmp); 
+  int i = 0;
+  for (; i<size; ++i){
+    if (array1[i] != array2[i])
+      return false;
+  }
+  return true;  
 }
--- projects/duju/dominion/test_helper.h
+++ projects/duju/dominion/test_helper.h
@@ -1,7 +1,13 @@
 #ifndef _TEST_HELPER_H 
 #define _TEST_HELPER_H 
 #include <stdbool.h>
+#include "dominion_helpers.h"
+
+int cmp(const void* a, const void* b);
+int* removeElementFromArray(int* originalArray,int size, int handPos);
+void printGameState(struct gameState* G, int player);
 void asserttrue(bool statement);
+bool isSameArray(int* array1, int* array2, int size);
 
 #endif
 
