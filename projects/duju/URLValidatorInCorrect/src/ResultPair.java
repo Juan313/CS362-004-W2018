06d1eb45bfe146df218563da30f6f9d1b05a4e27
Juan <duju@oregonstate.edu>
2018-03-16 13:34:23 -0500

    Final Project B initial commit

--- projects/duju/URLValidatorInCorrect/src/ResultPair.java
+++ projects/duju/URLValidatorInCorrect/src/ResultPair.java
@@ -21,12 +21,19 @@
  *
  * @version $Revision: 588091 $ $Date: 2007-10-24 17:17:42 -0700 (Wed, 24 Oct 2007) $
  */
+ 
  public class ResultPair {
-      public String item;
-      public boolean valid;
+     private String item;
+     private boolean valid;
 
-      public ResultPair(String item, boolean valid) {
-         this.item = item;
-         this.valid = valid;  //Weather the individual part of url is valid.
-      }
-   }
+     public ResultPair(String item, boolean valid) {
+        this.item = item;
+        this.valid = valid;  //Weather the individual part of url is valid.
+     }
+     public boolean getValidity(){
+   	  return this.valid;
+     }
+     public String getItem(){
+    	 return this.item;
+     }
+  }
